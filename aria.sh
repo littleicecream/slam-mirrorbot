007f75520e3dad36a924de65a33b016216c0735a
littleicecream <78819905+littleicecream@users.noreply.github.com>
2021-07-29 02:54:10 +0600

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -9,7 +9,7 @@ aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --check-certif
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
    --peer-id-prefix=-qB4350- --user-agent=qBittorrent/4.3.5 --peer-agent=qBittorrent/4.3.5 \
-   --disk-cache=64M --file-allocation=prealloc --continue=true \
+   --disk-cache=128M --file-allocation=prealloc --continue=true \
    --max-file-not-found=5 --max-tries=20 --auto-file-renaming=true \
    --bt-enable-lpd=true --seed-time=0.01 --seed-ratio=1.0 \
    --content-disposition-default-utf8=true --http-accept-gzip=true --reuse-uri=true --netrc-path=/usr/src/app/.netrc
